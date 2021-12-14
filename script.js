96c2bab31bdd8cea954357687a8654224fda21eb
Doston <abdullayev_doston9797@mail.ru>
2021-12-14 22:03:02 +0500

    JS1

---
+++ script.js
@@ -0,0 +1,125 @@
+/* console.log(1); 
+console. log("моя первая строка")
+console.log(true)
+console.log(false) */
+
+
+// типы данных (простые типы данных)
+
+/* 
+number - число
+String - срока
+boolean - логические типы данных  true - истина  false - ложь 
+ */
+
+
+
+
+
+/* ar countUsers = 5;
+console.log(countUsers)
+
+
+
+var number = 3;
+
+ */
+
+
+
+// Логические операторы
+
+/*  
++, -, *, /, 
+% - Остаток от деление
+> - Оператор больше
+< - оператор меньще
+<= - оператор меньше или равно
+>= - оператор больше или равно
+= - оператор равно
+!= - оператор не равно
+== - оператор сравнения
+=== - оператор строгого сравнения
+!== - строгое не равно
+&& - логический И
+!! - логический ИЛИ
+! - логический НЕ 
++ , конкатинация строк (склеивание строк)
+
+
+*/
+
+/* console.log(5 + 4);
+console.log(8-7);
+console.log(8-10)
+console.log(5 * 9)
+console.log(9 / 3)
+console.log(12 % 3)
+
+console.log("my name is " +  ); */
+
+/* return, function,  class, this - зарезервированные слова */
+
+
+/* alert("вызвал модалное окно") */
+
+
+/* var name = prompt("введите ваше имя")
+console.log(name) */
+
+
+/* var num = prompt("сколько вам лет")
+console.log(num) */
+
+/* console.log("10 + 15 = " + (10 + 15)); */
+
+/* var  num  = +prompt()
+console.log(num) */
+
+
+// undifined - тип данныъ который озночает не определенность
+
+/* var age;
+console.log(age) */
+
+
+
+// null - пустота
+
+var name = prompt("ваше имя");
+console.log("Mеня зовут " + name);
+
+var age = +prompt("ваш возраст")
+console.log("мне " + age)
+
+var num = +prompt("сколько будет 15 + 5 =")
+console.log("пример 1: " + "15 + 5 = 20 " + "(ваш ответ " + num + ")" )
+
+
+
+var number = +prompt("сколько будет 20 - 4 =")
+console.log("пример 2: " + "20 - 4 = 16 " + "(ваш ответ " + number + ")" )
+
+
+var example = +prompt("сколько будет 10 * 4 =")
+console.log("пример 3: " + "10 * 4 = 40 " + "(ваш ответ " + example + ")" )
+
+var rotate = +prompt("сколько будет 12 / 4 =")
+console.log("пример 4: " + "12 / 4 = 3 " + "(ваш ответ " + rotate + ")" )
+
+var potate = +prompt("сколько будет  10 % 3 =")
+console.log("пример 5: " + "10 % 3 = 1 " + "(ваш ответ " + potate + ")" )
+
+alert("откройте консоль " + "(через прсмотр кода)")
+
+
+
+
+
+
+
+
+
+
+
+
